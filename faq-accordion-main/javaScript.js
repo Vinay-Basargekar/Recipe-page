33e663718271f2d6cb89361f1e1d8a3f231378e5
Guffy-san <vinaybasargekar13@gmail.com>
2024-01-28 15:50:16 +0530

    deleted faq

--- faq-accordion-main/javaScript.js
+++
@@ -1,8 +0,0 @@
-function toggleAnswer(answerId) {
-	var answer = document.getElementById(answerId);
-	if (answer.style.display === "none") {
-		answer.style.display = "block";
-	} else {
-		answer.style.display = "none";
-	}
-}
